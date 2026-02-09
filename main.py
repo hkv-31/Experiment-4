3fe4242288fc6a6d72045b3352c7396adc001145
hkv-31 <135648477+hkv-31@users.noreply.github.com>
2026-02-09 09:12:31 +0530

    Add division functionality to calculator

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 import add as a 
 import subtract as s
 import multiply as m
+import divide as d
 
 #Calling add, subtract and multiply functions
 print("Addition= ", a.add(5, 6))
 print("Subtraction = ", s.subtract(7, 3))
 print("Multiplication = ", m.multiply(12, 24))
-
+print("Divide = ", d.divide(6, 3))
 
