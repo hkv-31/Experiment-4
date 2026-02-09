c42ed1f67be4afe3bccfb19a9c91678f519d8f07
HetanshiVora <hetanshi531@gmail.com>
2026-02-09 08:49:40 +0530

    Complete project ready

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #Calculator Program 
+import add as a 
+import subtract as s
+import multiply as m
+
+print("Addition= ", a.add(5, 6))
+print("Subtraction = ", s.subtract(7, 3))
+print("Multiplication = ", m.multiply(12, 24))
+
+
